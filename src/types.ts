63258695bbaf2b2d19a739b4f7d5f39a9978fb54
sureshrana522 <sureshranasureshrana522@gmail.com>
2026-02-11 10:12:48 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
 // src/types.ts
 
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  MANAGER = 'MANAGER',
+  MEASUREMENT = 'MEASUREMENT',
+  CUTTING = 'CUTTING',
+  SHIRT_MAKER = 'SHIRT_MAKER',
+  PANT_MAKER = 'PANT_MAKER',
+  COAT_MAKER = 'COAT_MAKER',
+  FINISHING = 'FINISHING',
+  PRESS = 'PRESS',
+  DELIVERY = 'DELIVERY'
+}
+
 export type SidebarItem = {
   id: string;
   label: string;
